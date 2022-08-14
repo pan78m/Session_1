e1d799e6700518549568b01b7c8aa2f47522a868
pan78m <aryanpankaj78@gmail.com>
2022-08-14 17:59:17 +0600

    first

--- problem_1.c
+++ problem_1.c
@@ -1,1 +1,9 @@
-//https://github.com/pan78m/Programming_Session_C_Languange.git
+// //https://github.com/pan78m/Programming_Session_C_Languange.git
+// 1. Write a C program that prints "I love programming..."
+// 2. Write a C program  that takes two integer number as user input and print them both.
+// 3. Write a C program  that takes two integer number as user input, subtract them and print the result
+// 4. Write a C program  that takes two float number as user input, multiply them and print the result
+// 5. Write a C program  that takes two double number as user input, divide them and print the result
+// 6. Write a C program  to find out area of a square    ( formula :   a * a )
+// 7. Write a C program  to find out area of a circle      ( formula :  3.1416 * r * r  )
+// 8. Write a C program  to find out area of a triangle ( এটার ফর্মুলা দিলাম না, আপনি একটু কষ্ট করেন )
